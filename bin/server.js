70f422fdefb2af662dc8fb98b0e91187a3c24c9b
krishpranav <krisna.pranav@gmail.com>
2021-04-26 13:34:57 +0530

    added function for simple rest server

--- bin/server.js
+++ bin/server.js
@@ -57,3 +57,34 @@ setInterval(function(){
     mapNetwork();
 }, program.update*1000);
 
+// simple rest server
+var server = http.createServer(function(req, res){
+    var path = req.url;
+//     debug( 'path: ' + path );
+
+	if ( path == '/' ){
+		if (req.method == 'GET') {
+			res.writeHead(200,{'Content-Type': 'text/html'});
+			res.write(page(db.getSortedList(),program.invert));
+// 			debug(db.get());
+			res.end();
+		}
+		else if (req.method == 'PUT') {
+			req.on('data', function(chunk) {
+				debug( chunk.toString() );
+			});
+		}
+	}
+    // return error
+	else {
+		// force users to /
+		debug("Wrong path " + path)
+		res.writeHead(404, "Not Found", {'Content-Type': 'text/html'});
+		res.write("Wrong path: use http://localhost:"+program.port+"\n");
+		res.end();
+	}
+});
+
+
+// start web server
+server.listen(program.port);
